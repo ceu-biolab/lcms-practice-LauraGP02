010a1a84da8fe5dbeec0ccb121c7dad8d3b42a61
Laura Gallego <laura.gallegoparedes@usp.ceu.es>
2025-05-18 13:32:49 +0200

    Entrega final de la práctica

--- src/main/java/adduct/Adduct.java
+++ src/main/java/adduct/Adduct.java
@@ -1,64 +1,24 @@
 package adduct;
 
+import adduct.AdductList;
+import org.drools.core.phreak.EagerPhreakBuilder;
+
+/**
+ * Utility class for performing mass calculations related to adducts in mass spectrometry.
+ * This class provides static methods to:
+ *      Calculate the monoisotopic mass of a compound from an experimental m/z and adduct.
+ *      Compute the parts-per-million (ppm) difference between experimental and theoretical masses.
+ *      Estimate tolerance windows based on a ppm value.
+ *
+ * @author laura
+ */
 public class Adduct {
-
-    /**
-     * Calculate the mass to search depending on the adduct hypothesis
-     *
-     * @param mz mz
-     * @param adduct adduct name ([M+H]+, [2M+H]+, [M+2H]2+, etc..)
-     *
-     * @return the monoisotopic mass of the experimental mass mz with the adduct @param adduct
-     */
-    public static Double getMonoisotopicMassFromMZ(Double mz, String adduct) {
-        Double massToSearch;
-        // !! TODO METHOD
-        // !! TODO Create the necessary regex to obtain the multimer (number before the M) and the charge (number before the + or - (if no number, the charge is 1).
-
-        /*
-        if Adduct is single charge the formula is M = m/z +- adductMass. Charge is 1 so it does not affect
-
-        if Adduct is double or triple charged the formula is M = ( mz +- adductMass ) * charge
-
-        if adduct is a dimer or multimer the formula is M =  (mz +- adductMass) / numberOfMultimer
-
-        return monoisotopicMass;
-
-         */
-        return null;
-    }
-
-    /**
-     * Calculate the mz of a monoisotopic mass with the corresponding adduct
-     *
-     * @param monoisotopicMass
-     * @param adduct adduct name ([M+H]+, [2M+H]+, [M+2H]2+, etc..)
-     *
-     * @return
-     */
-    public static Double getMZFromMonoisotopicMass(Double monoisotopicMass, String adduct) {
-        Double massToSearch;
-        // !! TODO METHOD
-        // !! TODO Create the necessary regex to obtain the multimer (number before the M) and the charge (number before the + or - (if no number, the charge is 1).
-
-        /*
-        if Adduct is single charge the formula is m/z = M +- adductMass. Charge is 1 so it does not affect
-
-        if Adduct is double or triple charged the formula is mz = M/charge +- adductMass
-
-        if adduct is a dimer or multimer the formula is mz = M * numberOfMultimer +- adductMass
-
-        return monoisotopicMass;
-
-         */
-        return null;
-    }
 
     /**
      * Returns the ppm difference between measured mass and theoretical mass
      *
-     * @param experimentalMass    Mass measured by MS
-     * @param theoreticalMass Theoretical mass of the compound
+     * @param experimentalMass Mass measured by MS
+     * @param theoreticalMass  Theoretical mass of the compound
      */
     public static int calculatePPMIncrement(Double experimentalMass, Double theoreticalMass) {
         int ppmIncrement;
@@ -70,17 +30,63 @@ public class Adduct {
     /**
      * Returns the ppm difference between measured mass and theoretical mass
      *
-     * @param measuredMass    Mass measured by MS
-     * @param ppm ppm of tolerance
+     * @param experimentalMass Mass measured by MS
+     * @param ppm          ppm of tolerance
      */
     public static double calculateDeltaPPM(Double experimentalMass, int ppm) {
         double deltaPPM;
-        deltaPPM =  Math.round(Math.abs((experimentalMass * ppm) / 1000000));
+        deltaPPM = Math.round(Math.abs((experimentalMass * ppm) / 1000000));
         return deltaPPM;
 
     }
 
 
+    /**
+     * Calculates the estimated monoisotopic mass for a given adduct.
+     * General formula: M = m/z * Z + A (rearranged depending on the adduct type).
+     *
+     * @param mz     experimental m/z value
+     * @param adduct name of the adduct (e.g., [M+H]+, [2M+Na]+, [M+2H]2+)
+     * @return estimated monoisotopic mass
+     */
+    public static Double getMonoisotopicMassFromMZ(Double mz, String adduct) {
+        /*
+        if Adduct is single charge the formula is M = mz +- adductMass. Charge is 1 so it does not affect
+        if Adduct is double or triple charged the formula is M =( mz - adductMass ) * charge
+        if adduct is a dimer the formula is M =  (mz - adductMass) / numberOfMultimer
+        return monoisotopicMass;
+         */
+        Double A = null;
 
+        if (AdductList.MAPMZPOSITIVEADDUCTS.containsKey(adduct)) {
+            A = AdductList.MAPMZPOSITIVEADDUCTS.get(adduct);
+        } else if (AdductList.MAPMZNEGATIVEADDUCTS.containsKey(adduct)) {
+            A = AdductList.MAPMZNEGATIVEADDUCTS.get(adduct);
+        }
 
+        if (A == null) {
+            return null;
+        }
+
+        int multimer = 1;
+        if (adduct.startsWith("[2M")) {
+            multimer = 2;
+        }
+
+        int charge = 1;
+        if (adduct.endsWith("2+") || adduct.endsWith("2-")) {
+            charge = 2;
+        }
+
+        // mz = (nM + adductMass) / charge
+        // (mz * charge) - adductMass = n * M
+        // M = ((mz * z) − adductMas) / n
+        if (A < 0){
+            return ((mz * charge) + A) / multimer;
+        } else {
+            return ((mz * charge) + A) / multimer;
+        }
+    }
 }
+
+
--- src/test/java/lipid/ElutionOrderTest.java
+++ src/test/java/lipid/ElutionOrderTest.java
@@ -1,36 +1,103 @@
 package lipid;
+
 import org.drools.ruleunits.api.RuleUnitInstance;
 import org.drools.ruleunits.api.RuleUnitProvider;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Description of the class.
+ *
+ * @author laura
+ */
 public class ElutionOrderTest {
-
-
     static final Logger LOG = LoggerFactory.getLogger(ElutionOrderTest.class);
+    /*@Test
+    public void shouldScoreBasedOnCarbonCountAndRT() {
+        // Creamos algunos lípidos y anotaciones con diferentes RT y carbonos
+        Lipid lipid1 = new Lipid(1, "TG 54:3", "C57H104O6", "TG", 54, 3); // MZ of [M+H]+=885.79057
+        Lipid lipid2 = new Lipid(2, "TG 52:3", "C55H100O6", "TG", 52, 3); // MZ of [M+H]+=857.75927
 
-    // !!TODO For the adduct detection both regular algorithms or drools can be used as far the tests are passed.
+        Annotation a1 = new Annotation(lipid1, 885.79056, 10E6, 10d, Set.of(new Peak(700.0, 100000)));
+        Annotation a2 = new Annotation(lipid2, 857.7593, 10E7, 9d, Set.of(new Peak(710.0, 100000)));
 
+        LipidScoreUnit unit = new LipidScoreUnit();
+        unit.getAnnotations().add(a1);//PASO FACTS
+        unit.getAnnotations().add(a2);
 
-    @Before
-    public void setup() {
-        // !! TODO Empty by now,you can create common objects for all tests.
+        RuleUnitInstance<LipidScoreUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(unit);
+        try {
+            instance.fire();
+
+            // Si las reglas se aplican correctamente, ambos deberían tener score 1
+            assertEquals(1, a1.getScore(),0.01);
+            assertEquals(1, a2.getScore(),0.01);
+        } finally {
+            instance.close();
+        }
     }
 
+    @Test
+    public void shouldScoreMultiplePairs() {
+        Lipid l1 = new Lipid(1, "L1", "C52H104O6", "TG", 52, 3);
+        Lipid l2 = new Lipid(2, "L2", "C54H108O6", "TG", 54, 3);
+        Lipid l3 = new Lipid(3, "L3", "C56H112O6", "TG", 56, 3);
 
-    /**
-     * Test to check the elution order of the lipids. The elution order is based on the number of carbons if the lipid type and the number of
-     * double bonds is the same. The larger the number of carbons, the longer the RT.
-     */
+        Annotation a1 = new Annotation(l1, 850.0, 1e6, 5.0, Set.of(new Peak(850.0, 1e6)));
+        Annotation a2 = new Annotation(l2, 870.0, 1e6, 6.0, Set.of(new Peak(870.0, 1e6)));
+        Annotation a3 = new Annotation(l3, 890.0, 1e6, 7.0, Set.of(new Peak(890.0, 1e6)));
+
+        LipidScoreUnit unit = new LipidScoreUnit();
+        unit.getAnnotations().add(a1);
+        unit.getAnnotations().add(a2);
+        unit.getAnnotations().add(a3);
+
+        RuleUnitInstance<LipidScoreUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(unit);
+        try {
+            instance.fire();
+
+            assertEquals(2, a1.getScore(), 0.01);
+            assertEquals(2, a2.getScore(), 0.01);
+            assertEquals(2, a3.getScore(), 0.01);
+
+        } finally {
+            instance.close();
+        }
+    }
+
+    @Test
+    public void shouldApplyPositiveAndNegativeScore() {
+        Lipid l1 = new Lipid(1, "L1", "C52H104O6", "TG", 52, 3);
+        Lipid l2 = new Lipid(2, "L2", "C54H108O6", "TG", 54, 3);
+
+        // L2 (más carbonos) eluye antes (RT=5), lo cual es incorrecto
+        Annotation a1 = new Annotation(l1, 850.0, 1e6, 6.0, Set.of(new Peak(850.0, 1e6))); // RT más tarde
+        Annotation a2 = new Annotation(l2, 870.0, 1e6, 5.0, Set.of(new Peak(870.0, 1e6))); // RT más pronto
+
+        LipidScoreUnit unit = new LipidScoreUnit();
+        unit.getAnnotations().add(a1);
+        unit.getAnnotations().add(a2);
+
+        RuleUnitInstance<LipidScoreUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(unit);
+        try {
+            instance.fire();
+
+            assertEquals(-1, a1.getScore(), 0.01);
+            assertEquals(-1, a2.getScore(), 0.01);
+
+        } finally {
+            instance.close();
+        }
+    }*/
+
+
     @Test
     public void score1BasedOnRTCarbonNumbers() {
         // Assume lipids already annotated
@@ -44,9 +111,9 @@ public class ElutionOrderTest {
         Lipid lipid1 = new Lipid(1, "TG 54:3", "C57H104O6", "TG", 54, 3); // MZ of [M+H]+ = 885.79057
         Lipid lipid2 = new Lipid(2, "TG 52:3", "C55H100O6", "TG", 52, 3); // MZ of [M+H]+ = 857.75927
         Lipid lipid3 = new Lipid(3, "TG 56:3", "C59H108O6", "TG", 56, 3); // MZ of [M+H]+ = 913.82187
-        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IoniationMode.POSITIVE);
-        Annotation annotation2 = new Annotation(lipid2, 857.7593, 10E7, 9d, IoniationMode.POSITIVE);
-        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 11d, IoniationMode.POSITIVE);
+        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IonizationMode.POSITIVE);
+        Annotation annotation2 = new Annotation(lipid2, 857.7593, 10E7, 9d, IonizationMode.POSITIVE);
+        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 11d, IonizationMode.POSITIVE);
 
         LOG.info("Insert data");
 
@@ -88,9 +155,9 @@ public class ElutionOrderTest {
         Lipid lipid1 = new Lipid(1, "TG 54:3", "C57H104O6", "TG", 54, 3); // MZ of [M+H]+ = 885.79057
         Lipid lipid2 = new Lipid(2, "TG 54:4", "C57H102O6", "TG", 54, 4); // MZ of [M+H]+ = 883.77492
         Lipid lipid3 = new Lipid(3, "TG 54:2", "C57H106O6", "TG", 54, 2); // MZ of [M+H]+ = 887.80622
-        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IoniationMode.POSITIVE);
-        Annotation annotation2 = new Annotation(lipid2, 883.77492, 10E7, 9d, IoniationMode.POSITIVE);
-        Annotation annotation3 = new Annotation(lipid3, 887.80622, 10E5, 11d, IoniationMode.POSITIVE);
+        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IonizationMode.POSITIVE);
+        Annotation annotation2 = new Annotation(lipid2, 883.77492, 10E7, 9d, IonizationMode.POSITIVE);
+        Annotation annotation3 = new Annotation(lipid3, 887.80622, 10E5, 11d, IonizationMode.POSITIVE);
 
         LOG.info("Insert data");
 
@@ -133,9 +200,9 @@ public class ElutionOrderTest {
         Lipid lipid1 = new Lipid(1, "PI 34:0", "C43H83O13P", "PI", 54, 0); // MZ of [M+H]+ = 839.56441
         Lipid lipid2 = new Lipid(2, "PG 34:0", "C40H79O10P", "PG", 54, 0); // MZ of [M+H]+ = 751.54836
         Lipid lipid3 = new Lipid(3, "PC 34:0", "C42H84NO8P", "PC", 54, 0); // MZ of [M+H]+ = 762.60073
-        Annotation annotation1 = new Annotation(lipid1, 839.5644179056, 10E6, 10d, IoniationMode.POSITIVE);
-        Annotation annotation2 = new Annotation(lipid2, 751.54836, 10E7, 9d, IoniationMode.POSITIVE);
-        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 11d, IoniationMode.POSITIVE);
+        Annotation annotation1 = new Annotation(lipid1, 839.5644179056, 10E6, 10d, IonizationMode.POSITIVE);
+        Annotation annotation2 = new Annotation(lipid2, 751.54836, 10E7, 9d, IonizationMode.POSITIVE);
+        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 11d, IonizationMode.POSITIVE);
 
         LOG.info("Insert data");
 
@@ -179,9 +246,9 @@ public class ElutionOrderTest {
         Lipid lipid1 = new Lipid(1, "PI 34:0", "C43H83O13P", "PI", 54, 0); // MZ of [M+H]+ = 839.56441
         Lipid lipid2 = new Lipid(2, "PG 34:0", "C40H79O10P", "PG", 54, 0); // MZ of [M+H]+ = 751.54836
         Lipid lipid3 = new Lipid(3, "PC 34:0", "C42H84NO8P", "PC", 54, 0); // MZ of [M+H]+ = 762.60073
-        Annotation annotation1 = new Annotation(lipid1, 839.5644179056, 10E6, 10d, IoniationMode.POSITIVE);
-        Annotation annotation2 = new Annotation(lipid2, 751.54836, 10E7, 9d, IoniationMode.POSITIVE);
-        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 8d, IoniationMode.POSITIVE);
+        Annotation annotation1 = new Annotation(lipid1, 839.5644179056, 10E6, 10d, IonizationMode.POSITIVE);
+        Annotation annotation2 = new Annotation(lipid2, 751.54836, 10E7, 9d, IonizationMode.POSITIVE);
+        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 8d, IonizationMode.POSITIVE);
 
         LOG.info("Insert data");
 
@@ -223,9 +290,9 @@ public class ElutionOrderTest {
         Lipid lipid1 = new Lipid(1, "TG 54:3", "C57H104O6", "TG", 54, 3); // MZ of [M+H]+ = 885.79057
         Lipid lipid2 = new Lipid(2, "TG 54:4", "C57H102O6", "TG", 54, 4); // MZ of [M+H]+ = 883.77492
         Lipid lipid3 = new Lipid(3, "TG 54:2", "C57H106O6", "TG", 54, 2); // MZ of [M+H]+ = 887.80622
-        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IoniationMode.POSITIVE);
-        Annotation annotation2 = new Annotation(lipid2, 883.77492, 10E7, 9d, IoniationMode.POSITIVE);
-        Annotation annotation3 = new Annotation(lipid3, 887.80622, 10E5, 8d, IoniationMode.POSITIVE);
+        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IonizationMode.POSITIVE);
+        Annotation annotation2 = new Annotation(lipid2, 883.77492, 10E7, 9d, IonizationMode.POSITIVE);
+        Annotation annotation3 = new Annotation(lipid3, 887.80622, 10E5, 8d, IonizationMode.POSITIVE);
 
         LOG.info("Insert data");
 
@@ -266,9 +333,9 @@ public class ElutionOrderTest {
         Lipid lipid1 = new Lipid(1, "TG 54:3", "C57H104O6", "TG", 54, 3); // MZ of [M+H]+ = 885.79057
         Lipid lipid2 = new Lipid(2, "TG 52:3", "C55H100O6", "TG", 52, 3); // MZ of [M+H]+ = 857.75927
         Lipid lipid3 = new Lipid(3, "TG 56:3", "C59H108O6", "TG", 56, 3); // MZ of [M+H]+ = 913.82187
-        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IoniationMode.POSITIVE);
-        Annotation annotation2 = new Annotation(lipid2, 857.7593, 10E7, 9d, IoniationMode.POSITIVE);
-        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 8d, IoniationMode.POSITIVE);
+        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IonizationMode.POSITIVE);
+        Annotation annotation2 = new Annotation(lipid2, 857.7593, 10E7, 9d, IonizationMode.POSITIVE);
+        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 8d, IonizationMode.POSITIVE);
 
         LOG.info("Insert data");
 
@@ -295,5 +362,5 @@ public class ElutionOrderTest {
 
     }
 
-
 }
+
